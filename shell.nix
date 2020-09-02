1fa7f2fbc4567221737bcb56b7f4329c3bf5b2ec
Martin Toman <whomenope@outlook.com>
2020-09-02 09:28:52 +0200

    Niv lorri direnv

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ sources ? import ./nix/sources.nix }:
+
+with { overlay = _: pkgs: {
+  niv          = import sources.niv { };
+}; };
+
+with import sources.nixpkgs {
+  overlays = [ overlay ];
+  config = { };
+};
+
+mkShell {
+  buildInputs = [
+    git
+  ];
+}
