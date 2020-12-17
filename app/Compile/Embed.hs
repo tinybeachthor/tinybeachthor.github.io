03516dac9c9a85255a468ddc50805074528dbed7
Martin Toman <toman.martin@live.com>
2020-12-17 16:43:27 +0100

    Embed youtu.be links

--- app/Compile/Embed.hs
+++ app/Compile/Embed.hs
@@ -6,6 +6,7 @@ module Compile.Embed
 where
 
 import           CMarkGFM
+import           Data.Char (isAlpha, isNumber)
 import           Data.Maybe                     ( fromJust )
 
 import qualified Data.Text                     as T
@@ -14,11 +15,20 @@ embedNode :: Node -> Node
 embedNode (Node pos type_ ns) = Node pos (transformNode type_) (map embedNode ns)
 
 transformNode :: NodeType -> NodeType
-transformNode img@(IMAGE url title) =
-  if youtubePrefix `T.isPrefixOf` url
-     then renderYoutube url title
-     else img
+transformNode img@(IMAGE url title)
+  | youtuPrefix   `T.isPrefixOf` url = renderYoutu   url title
+  | youtubePrefix `T.isPrefixOf` url = renderYoutube url title
+  | otherwise                        = img
 transformNode n = n
+
+youtuPrefix :: T.Text
+youtuPrefix = "https://youtu.be/"
+
+renderYoutu :: Url -> Title -> NodeType
+renderYoutu url _ = HTML_INLINE formatted
+  where
+    v = (T.replace "t=" "start=") . fromJust . (T.stripPrefix youtuPrefix) $ url
+    formatted = "<div class='video-container'><iframe src='https://www.youtube.com/embed/" <> v <> "'></iframe></div>"
 
 youtubePrefix :: T.Text
 youtubePrefix = "https://www.youtube.com/watch?v="
