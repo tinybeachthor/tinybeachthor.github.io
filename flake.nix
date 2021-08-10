aa8c5f99aa222cda2f7b0a1d5fd9d264946530a2
Martin Toman <toman.martin@live.com>
2021-08-09 20:55:19 -0700

    Remove flake-utils, cleanup flake

--- flake.nix
+++ flake.nix
@@ -1,25 +1,36 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
     haskellNix.url = "github:input-output-hk/haskell.nix/master";
   };
-  outputs = { self, flake-utils, nixpkgs, haskellNix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs {
-          inherit system;
-          overlays = [ haskellNix.overlay ];
-        });
-        project = (import ./default.nix { inherit pkgs; });
-        blog4 = project.blog4;
-      in rec {
-        defaultPackage = blog4.components.exes.blog4;
-        packages = {
+  outputs = { self, nixpkgs, haskellNix }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+
+      # Function to generate a set based on supported systems
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ haskellNix.overlay ];
+      });
+
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          project = (import ./default.nix { inherit pkgs; });
+          blog4 = project.blog4;
+        in {
           plan-nix = project.plan-nix;
           materialize = project.updateMaterialized;
-          blog4 = defaultPackage;
-        };
-        devShell = import ./shell.nix { inherit pkgs; };
+          blog4 = blog4.components.exes.blog4;
+        });
+      defaultPackage = forAllSystems (system: self.packages.${system}.blog4);
+
+      devShell = forAllSystems (system: import ./shell.nix {
+        pkgs = nixpkgsFor.${system};
       });
+    };
 }
