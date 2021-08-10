b2d43320e638a04cb4382a2bd0a647c9b3dc7cd3
Martin Toman <toman.martin@live.com>
2021-08-09 20:48:28 -0700

    Remove tinybeachthor NUR input

--- flake.nix
+++ flake.nix
@@ -2,18 +2,14 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
-    tinybeachthor = {
-      url = "github:tinybeachthor/nur-packages/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     haskellNix.url = "github:input-output-hk/haskell.nix/master";
   };
-  outputs = { self, flake-utils, nixpkgs, tinybeachthor, haskellNix }:
+  outputs = { self, flake-utils, nixpkgs, haskellNix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs {
           inherit system;
-          overlays = [ tinybeachthor.overlay haskellNix.overlay ];
+          overlays = [ haskellNix.overlay ];
         });
         project = (import ./default.nix { inherit pkgs; });
         blog4 = project.blog4;
